98f41d58571c4dfde9e01130827028347f4eb8cd
Blackbirdxx <superblackbirdxx@mail.ru>
2022-09-11 22:48:40 +0300

    Задача 04

---
+++ Task04/Task04.py
@@ -0,0 +1,34 @@
+# 4 - Реализуйте выдачу случайного числа
+# не использовать random.randint и вообще библиотеку random
+# Можете использовать xor, биты, библиотеку time или datetime (миллисекунды или наносекунды) - для задания случайности
+# Учтите, что есть диапазон: от(минимальное) и до (максимальное)
+
+
+import math
+import time
+import os
+os.system("cls")
+
+print('Программа выдающая случайное число.')
+
+def number_input(input_string):
+    while type:
+        digit = input(input_string)
+        try:
+            digit = int(digit)
+            return digit
+        except ValueError:
+            print("Введено неверное значение. Только числа!!!")
+
+min_range = number_input('Введите минимальное значение  :  ')
+max_range = number_input('Введите максимальное значение  :  ')
+
+def LinearCR_fill_array(seed,min_value,max_value):
+    a=1
+    b=10
+    result = math.ceil(math.fmod(a*math.ceil(seed)+b,max_value))
+    if result < min_value:
+        result += min_value
+    return result
+   
+print(LinearCR_fill_array(time.time(),min_range, max_range))
